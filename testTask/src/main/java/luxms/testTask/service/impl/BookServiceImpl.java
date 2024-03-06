d7a1ff90fcbda9312b1daf9ad09ea017a6895eb9
ikomers <komers.for@gmail.com>
2024-03-06 12:35:50 +0300

    add filter for exlude NPE

--- testTask/src/main/java/luxms/testTask/service/impl/BookServiceImpl.java
+++ testTask/src/main/java/luxms/testTask/service/impl/BookServiceImpl.java
@@ -29,6 +29,19 @@ public class BookServiceImpl implements BookService {
             bookStream = bookStream.filter(book -> book.getPublicationDate() != null && book.getPublicationDate().getYear() == year);
         }
 
+        // Adding a filter to exclude books with null values for the specified column
+        bookStream = bookStream.filter(book -> {
+            switch (column) {
+                case "book": return book.getTitle() != null;
+                case "author": return book.getAuthors() != null;
+                case "numPages": return book.getNumPages() != null;
+                case "publicationDate": return book.getPublicationDate() != null;
+                case "rating": return book.getRatingScore() != null;
+                case "numberOfVoters": return book.getNumRatings() != null;
+                default: throw new IllegalArgumentException("Invalid column name");
+            }
+        });
+
         Sort.Direction direction = sort.equalsIgnoreCase("DESC") ? Sort.Direction.DESC : Sort.Direction.ASC;
         Comparator<Book> comparator = switch (column) {
             case "book" -> Comparator.comparing(Book::getTitle);
@@ -44,11 +57,9 @@ public class BookServiceImpl implements BookService {
                 .limit(10)
                 .collect(Collectors.toList());
 
-
         if (direction == Sort.Direction.DESC) {
             Collections.reverse(result);
         }
         return result;
     }
 }
-
